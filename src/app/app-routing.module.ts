fe82634ed095de8b06fda8cc161d314cd393fcac
Lizie <lizi_23_6@hotmail.com>
2020-02-01 11:50:13 -0500

    User management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  // { path: 'search', component: SearchComponent },
+  // { path: 'search/:term', component: SearchComponent },
+  // { path: 'hero/:id/:page', component: HeroeComponent },
+  // { path: 'hero/:id/:page/:term', component: HeroeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'users' },
+  { path: '**', pathMatch: 'full', redirectTo: 'users' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { DataTablesModule } from 'angular-datatables';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]

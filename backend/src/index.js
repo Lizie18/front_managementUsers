302e338ee917341083139be98430dd7820890225
Lizie <lizi_23_6@hotmail.com>
2020-02-08 19:06:28 -0500

    Frontend y backend 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors'); 
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 app.use(morgan('dev'));
+app.use(cors());
 
 // Middlewares
 app.use(express.json());
--- frontend/src/app/components/users/services/user.service.ts
+++ frontend/src/app/components/users/services/user.service.ts
@@ -7,12 +7,20 @@ import { HttpClient } from '@angular/common/http';
 export class UserService {
   public url: string;
   constructor(
-    private http: HttpClient;
+    private http: HttpClient
   ) {
     this.url = 'http://localhost:3000/api';
   }
 
   listAllUsers() {
     return this.http.get(this.url);
+  }
+
+  getUser(id) {
+   return this.http.get(`${this.url}/${id}`); 
+  }
+
+  addUser(params) {
+   return this.http.post(this.url, params); 
   }
 }
